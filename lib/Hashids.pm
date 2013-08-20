361470688d51e0ee7e35a036377ded65c3f0a6f8
Zak B. Elep <zakame@zakame.net>
2013-08-20 17:58:46 +0800

    Correct synopsis
    Forgot to specify salt key, lel

--- lib/Hashids.pm
+++ lib/Hashids.pm
@@ -326,7 +326,7 @@ Hashids - generate short hashes from numbers
 =head1 SYNOPSIS
 
     use Hashids;
-    my $hashids = Hashids->new("this is my salt");
+    my $hashids = Hashids->new(salt => "this is my salt");
 
     # encrypt a single number
     my $hash = $hashids->encrypt(123);          # 'a79'
